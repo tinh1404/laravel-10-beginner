242e44852a7c645ad8d5be0e2cbddae0c3ecf0fe
THi Tinh <thitrungtinh1@gmail.com>
2024-02-28 14:03:13 +0700

    create avatar in info

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -18,6 +18,12 @@
         @method('patch')
 
         <div>
+            <x-input-label for="name" value="Avatar" />
+            <x-text-input id="avatar" avatar="avatar" type="file" class="mt-1 block w-full" :value="old('avatar', $user->avatar)" required autofocus autocomplete="avatar" />
+            <x-input-error class="mt-2" :messages="$errors->get('avatar')" />
+        </div> 
+
+        <div>
             <x-input-label for="name" :value="__('Name')" />
             <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autofocus autocomplete="name" />
             <x-input-error class="mt-2" :messages="$errors->get('name')" />
@@ -61,4 +67,5 @@
             @endif
         </div>
     </form>
+    
 </section>
