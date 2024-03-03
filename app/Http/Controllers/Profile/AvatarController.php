f17f797384002b46e3ac39a1ce233ef333e5d495
THi Tinh <thitrungtinh1@gmail.com>
2024-03-03 13:06:34 +0700

    upload image delete when image is duplicate and storage file

--- app/Http/Controllers/Profile/AvatarController.php
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -4,13 +4,19 @@ namespace App\Http\Controllers\Profile;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UpdateAvatarRequest;
-
+use Illuminate\Support\Facades\Storage;
 
 class AvatarController extends Controller
 {
     public function update(UpdateAvatarRequest $request)
     {
-        $path = $request->file('avatar')->store('avatars','public');
+        $path = Storage::disk('public')->put('avatars', $request->file('avatar'));
+        // $path = $request->file('avatar')->store('avatars','public');
+
+        if($oldAvatar = $request->user()->avatar) {
+            Storage::disk('public')->delete($oldAvatar);
+        }
+
         auth()->user()->update(['avatar' => $path]);
 
         // store avatar
